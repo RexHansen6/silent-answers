05b0bac0e4cc9df798025a058f5631391f179b0c
MarinaEddie <muminagresxm@outlook.com>
2025-11-01 18:30:00 +0800

    refactor: improve contract import organization

--- contracts/EncryptedSurvey.sol
+++ contracts/EncryptedSurvey.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+// Enhanced import structure for better organization
 import {FHE, euint32, externalEuint32} from "@fhevm/solidity/lib/FHE.sol";
 import {SepoliaConfig} from "@fhevm/solidity/config/ZamaConfig.sol";
 import "encrypted-types/EncryptedTypes.sol";
